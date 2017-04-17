e37b87802b0c0347b36364c646de49f07aeb8652
zhouxudong <z_zxdong@163.com>
2017-04-17 10:24:51 +0800

    chat app entry

--- app/main.js
+++ app/main.js
@@ -1,8 +1,7 @@
 import Vue from "vue"
-
-var app = new Vue({
+import App from "./App.vue"
+new Vue({
     el: "#app",
-    data: {
-        message: 'Hello Vue'
-    }
-})
+    template: "<App />",
+    components: { App }
+});
